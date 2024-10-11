008b3dcf5bbc97001440210b6e1e45ad43ffed3e
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-10-11 17:29:51 +0200

    Update operadors.js

--- exercicis/operadors.js
+++ exercicis/operadors.js
@@ -16,4 +16,20 @@ b = 4;
 c = c + 3 - b + 2;
 console.log(a, b, c);
 
-// Fins ara
+// 4.3.- Quins valors s'escriuran a la consola després d'executar aquest codi?
+
+a = 5; //5
+b = 18; //18
+c = 15; //15
+d = 22; //22
+a = a + 10; //15
+console.log(a);
+b = b + 5 - c; //18 + 5 - 15 = 8
+c = c + 4 + b; //15 + 4 + 8 = 27
+d = d + b + a; //...
+a = a + 1;
+b = b + c;
+c = b + c;
+d = b + b;
+
+console.log(a,b,c,d);
