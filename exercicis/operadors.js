e445efb7a211e2ae7056f4230215c7751546955f
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-10-11 16:09:32 +0200

    operadors exer

---
+++ exercicis/operadors.js
@@ -0,0 +1,17 @@
+// 4.1.- Quins valors s'escriuran a la consola després d'executar aquest codi?
+let a = 25;
+let b = 15 + a;
+let c = b * 2;
+console.log(a, b, c);
+
+// 4.2.- Quins valors s'escriuran a la consola després d'executar aquest codi?
+a = 10; //10
+b = 20; //20
+c = 5; //5
+a = a + 3; //13
+b = b + 4 - a; //11
+c = a + b + c;
+a = a + c;
+b = 4;
+c = c + 3 - b + 2;
+console.log(a, b, c);
