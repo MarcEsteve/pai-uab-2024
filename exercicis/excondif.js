daaf2a80e7e5b1d1cb534b6d0ce07b3fbc6d68b0
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-10-18 16:10:31 +0200

    condicionals if exercicis

---
+++ exercicis/excondif.js
@@ -0,0 +1,15 @@
+// 13.1.- Escriu un programa que donat un número llegit amb prompt escrigui el missatge "el número llegit és negatiu" si el número és més petit que zero. 
+/*
+dskfdsf
+sdgfdsf
+*/
+
+let numeroNegatiu = parseInt(prompt("Introdueix un número negatiu:"));
+
+if (numeroNegatiu<0) {
+    document.write("El número llegit és negatiu");
+    console.log("El número llegit és negatiu");
+   
+}
+document.getElementById('resposta1').innerHTML = "Això és un altre output primer";
+document.getElementById('resposta2').innerHTML = "Això és un altre output sota el subtitular";
