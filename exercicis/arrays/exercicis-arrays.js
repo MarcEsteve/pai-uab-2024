3b57a33a9c1b7facafebb9552902cb9043243998
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-10-25 18:54:14 +0200

    Update exercicis-arrays.js

--- exercicis/arrays/exercicis-arrays.js
+++ exercicis/arrays/exercicis-arrays.js
@@ -1,5 +1,7 @@
-// 21.1.- Fes un programa que faci el següent: ● Defineix una variable de tipus taula, buida. ● Afegeixi el valor “Hola” a la posició 0. ● Afegeixi el valor “Adeu” a la posició 1. ● Escrigui a la consola el contingut de la taula. 
+// 21.1.- Fes un programa que faci el següent: ● Defineix una variable de tipus taula, buida. ● Afegeixi el valor “Hola” a la posició 0. ● Afegeixi el valor “Adeu” a la posició 1. ● Escrigui a la consola el contingut de la taula.
 
 let arai = [];
-arai [0]= "Hola";
-arai [1]= "Adéu";
+arai[0] = "Hola";
+arai[1] = "Adéu";
+
+// 21.2.- Al programa anterior, afegeix les instruccions necessàries (després del console.log()) per a fer el següent: ● Afegeixi el valor “Nom” a la posició 2 de la taula. ● Escrigui a la consola el contingut de la taula. ● Modifiqui el valor emmagatzemat a la posició 1 de la taula, guardant el valor “, “ ● Escrigui a la consola el contingut de la taula.
