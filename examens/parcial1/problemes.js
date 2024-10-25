504ed4a5497930f81a7fd0662fb1b9ec8ce0a4f8
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-10-25 16:03:13 +0200

    codi extra examens

--- examens/parcial1/problemes.js
+++ examens/parcial1/problemes.js
@@ -3,12 +3,13 @@
 let numero1 = parseFloat(prompt("Introdueix un número:"));
 
 if (numero1 > 0) {
-    console.log("El número és positiu");
+  console.log("El número és positiu");
 } else if (numero1 < 0) {
-    console.log("El número és negatiu");
+  console.log("El número és negatiu");
 } else {
-    console.log("El número és zero");
+  console.log("El número és zero");
 }
+
 
 // 2.- (2 punts) Escriu un programa que, donat un número entre 1 i 7, escrigui a la consola el dia de la setmana corresponent (1 = dilluns, 2 = dimarts, etc.). El programa he de controlar que el número estigui entre 1 i 7
 
@@ -17,23 +18,23 @@ if (numero1 > 0) {
 let numero2 = parseInt(prompt("Introdueix un número entre 1 i 7:"));
 
 if (numero2 >= 1 && numero2 <= 7) {
-    if (numero2 === 1) {
-        console.log("Dilluns");
-    } else if (numero2 === 2) {
-        console.log("Dimarts");
-    } else if (numero2 === 3) {
-        console.log("Dimecres");
-    } else if (numero2 === 4) {
-        console.log("Dijous");
-    } else if (numero2 === 5) {
-        console.log("Divendres");
-    } else if (numero2 === 6) {
-        console.log("Dissabte");
-    } else if (numero2 === 7) {
-        console.log("Diumenge");
-    }
+  if (numero2 === 1) {
+    console.log("Dilluns");
+  } else if (numero2 === 2) {
+    console.log("Dimarts");
+  } else if (numero2 === 3) {
+    console.log("Dimecres");
+  } else if (numero2 === 4) {
+    console.log("Dijous");
+  } else if (numero2 === 5) {
+    console.log("Divendres");
+  } else if (numero2 === 6) {
+    console.log("Dissabte");
+  } else if (numero2 === 7) {
+    console.log("Diumenge");
+  }
 } else {
-    console.log("Error: el número introduït ha d'estar entre 1 i 7.");
+  console.log("Error: el número introduït ha d'estar entre 1 i 7.");
 }
 
 // OPCIÓ 2 amb if + bucle while previ
@@ -42,62 +43,62 @@ let numero21 = parseInt(prompt("Introdueix un número entre 1 i 7:"));
 
 //Bucle per sol·lucionar la segona part on diu que controlem l'entrada entre 1 i 7
 while (numero21 < 1 || numero21 > 7) {
-    numero21 = parseInt(prompt("Error! Introdueix un número entre 1 i 7:"));
+  numero21 = parseInt(prompt("Error! Introdueix un número entre 1 i 7:"));
 }
 
 if (numero21 === 1) {
-    console.log("Dilluns");
+  console.log("Dilluns");
 } else if (numero21 === 2) {
-    console.log("Dimarts");
+  console.log("Dimarts");
 } else if (numero21 === 3) {
-    console.log("Dimecres");
+  console.log("Dimecres");
 } else if (numero21 === 4) {
-    console.log("Dijous");
+  console.log("Dijous");
 } else if (numero21 === 5) {
-    console.log("Divendres");
+  console.log("Divendres");
 } else if (numero21 === 6) {
-    console.log("Dissabte");
+  console.log("Dissabte");
 } else if (numero21 === 7) {
-    console.log("Diumenge");
+  console.log("Diumenge");
 }
 
 // OPCIÓ 3 amb switch + bucle for previ
 
 let numero22;
 
-for (;;) {  // Bucle infinit
-    numero22 = parseInt(prompt("Introdueix un número entre 1 i 7:"));
-    if (numero22 >= 1 && numero22 <= 7) {
-        break;  // Sortim del bucle si el número és vàlid
-    } else {
-        console.log("Error! Introdueix un número entre 1 i 7:");
-    }
+for (;;) {
+  // Bucle infinit
+  numero22 = parseInt(prompt("Introdueix un número entre 1 i 7:"));
+  if (numero22 >= 1 && numero22 <= 7) {
+    break; // Sortim del bucle si el número és vàlid
+  } else {
+    console.log("Error! Introdueix un número entre 1 i 7:");
+  }
 }
 
 switch (numero22) {
-    case 1:
-        console.log("Dilluns");
-        break;
-    case 2:
-        console.log("Dimarts");
-        break;
-    case 3:
-        console.log("Dimecres");
-        break;
-    case 4:
-        console.log("Dijous");
-        break;
-    case 5:
-        console.log("Divendres");
-        break;
-    case 6:
-        console.log("Dissabte");
-        break;
-    case 7:
-        console.log("Diumenge");
-        break;
+  case 1:
+    console.log("Dilluns");
+    break;
+  case 2:
+    console.log("Dimarts");
+    break;
+  case 3:
+    console.log("Dimecres");
+    break;
+  case 4:
+    console.log("Dijous");
+    break;
+  case 5:
+    console.log("Divendres");
+    break;
+  case 6:
+    console.log("Dissabte");
+    break;
+  case 7:
+    console.log("Diumenge");
+    break;
 }
-
 
 // 3.- (2 punts) Escriu un programa en JavaScript que demani a l'usuari un número N i mostri a la consola tots els números de l'1 fins a N. Pots fer-ho utilitzant tant un bucle for com un bucle while.
 
@@ -106,7 +107,7 @@ switch (numero22) {
 let nFor = parseInt(prompt("Introdueix un número N:"));
 
 for (let i = 1; i <= nFor; i++) {
-    console.log(i);
+  console.log(i);
 }
 
 // Opció amb WHILE
@@ -115,6 +116,6 @@ let nWhile = parseInt(prompt("Introdueix un número N:"));
 let i = 1;
 
 while (i <= nWhile) {
-    console.log(i);
-    i++;
+  console.log(i);
+  i++;
 }
