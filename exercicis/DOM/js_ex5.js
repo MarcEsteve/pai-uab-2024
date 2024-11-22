972c2b8277d89f1966a803b602aff185b6c81a7f
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-11-22 15:08:17 +0100

    DOM exer

--- exercicis/DOM/js_ex5.js
+++ exercicis/DOM/js_ex5.js
@@ -1,44 +1,29 @@
 /****** Exercici 5.1 ******/
 // a.	Número d'enllaços que hi ha a la pàgina
-function e51a(){
-	alert("hola");
+function e51a() {
+  alert("hola");
 }
+
 // b.	Adreça (URL) del penúltim enllaç
-function e51b(){
+function e51b() {}
 
-}
 //c.	Numero d'enllaços que ho fan a http://www.uab.cat
-function e51c(){
+function e51c() {}
 
-}
 //d.	Número d'enllaços al tercer paràgraf.
-function e51d(){
-
-}
+function e51d() {}
 /****** Exercici 5.2 ******/
 // a. Al final de dels paràgrafs existents.
-function e52a(){ 
-
-}
+function e52a() {}
 // b. Entre el segon i el tercer paràgraf.
-function e52b(){
-
-}
-
+function e52b() {}
 
 /****** Exercici 5.3 ******/
-// Afegir "Salve Mundi!" a la llista en penúltima posició. 
-function e53a(){	
-
-}
+// Afegir "Salve Mundi!" a la llista en penúltima posició.
+function e53a() {}
 // Canviar l'estil de la llista
-function e53b(){
-	
-}
-
+function e53b() {}
 
 /****** Exercici 5.4 ******/
 // Amagar el 2on paràgraf
-function e54(){
-
-}
+function e54() {}
